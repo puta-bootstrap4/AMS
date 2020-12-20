8d994c8b965c88cde7c6d4f74567a9251aa54188
puta-bootstrap4 <katsuyuki05170517@gmail.com>
2020-12-20 02:26:00 +0000

    c

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_17_074938) do
+ActiveRecord::Schema.define(version: 2020_12_19_164158) do
 
   create_table "app_januaries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.time "start_time"
@@ -35,8 +35,8 @@ ActiveRecord::Schema.define(version: 2020_12_17_074938) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
     t.boolean "admin", default: false
+    t.string "username"
     t.string "password"
     t.string "email"
   end
