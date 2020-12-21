843ed27050a532873e239aeb89ecd4436ace6f1e
puta-bootstrap4 <katsuyuki05170517@gmail.com>
2020-12-21 19:43:31 +0900

    8th commit

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   post "users/create" => "users#create"
   post "login" => "users#login"
   post "logout"=> "users#logout"
-  post "users" => "users#new"
+  post "users/new" => "users#new"
   post "users/:id/destroy" =>"users#destroy"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
